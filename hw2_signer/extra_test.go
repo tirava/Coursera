c9a5f7c0cf752a67bd9b8c5987a5007b4603024a
Eugene Klimov <tirava@gmail.com>
2019-07-07 00:00:10 +0300

    Work - 1

--- hw2_signer/extra_test.go
+++ hw2_signer/extra_test.go
@@ -24,21 +24,17 @@ func TestByIlia(t *testing.T) {
 	var recieved uint32
 	freeFlowJobs := []job{
 		job(func(in, out chan interface{}) {
-			println("joba 1")
 			out <- uint32(1)
 			out <- uint32(3)
 			out <- uint32(4)
 		}),
 		job(func(in, out chan interface{}) {
-			println("joba 2")
 			for val := range in {
 				out <- val.(uint32) * 3
 				time.Sleep(time.Millisecond * 100)
-				println("joba 2 val", val)
 			}
 		}),
 		job(func(in, out chan interface{}) {
-			println("joba 3")
 			for val := range in {
 				fmt.Println("collected", val)
 				atomic.AddUint32(&recieved, val.(uint32))
--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -6,54 +6,25 @@ import "sync"
 //
 //}
 
-//func Worker(job job, in, out chan interface{}, wg *sync.WaitGroup) {
-//	defer wg.Done()
-//	println("start Worker", in)
-//	job(in, out)
-//	println("end Worker", in)
-//	//close(in)
-//}
-
-func Worker(joba chan job, in, out chan interface{}, wg *sync.WaitGroup) {
-	//defer wg.Done()
-	//in := make(chan interface{}, MaxInputDataLen)
-	//out := make(chan interface{}, MaxInputDataLen)
-	println("bef joba")
-	for job := range joba {
-		println("worker joba")
-		job(in, out)
-		//close(out)
-	}
-	println("exit worker")
+func Worker(job job, in, out chan interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(out)
+	job(in, out)
 }
 
 func ExecutePipeline(jobs ...job) {
 
 	in := make(chan interface{}, MaxInputDataLen)
-	out := make(chan interface{}, MaxInputDataLen)
 	wg := &sync.WaitGroup{}
 
-	workerInput := make(chan job, len(jobs))
-	//for range jobs {
-	//	wg.Add(1)
-	go Worker(workerInput, in, out, wg)
-	//}
-
 	for _, job := range jobs {
-		//println("bef input")
-		//in, out = out, in
-		//wg.Add(1)
-		//go Worker(job, in, out, wg)
-		//go Worker(job)
-		//runtime.Gosched()
-		workerInput <- job
-		//in, out = out, in
-		//println("after input")
+		out := make(chan interface{}, MaxInputDataLen)
+		wg.Add(1)
+		go Worker(job, in, out, wg)
+		in = out
 	}
 
-	close(workerInput)
-
-	//wg.Wait()
+	wg.Wait()
 }
 
 func SingleHash(in, out chan interface{}) {
