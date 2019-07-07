f40d33ec2fb4eb9886c2ad439ca8229032edee48
Eugene Klimov <tirava@gmail.com>
2019-07-07 13:27:25 +0300

    Work - 2^ functions linear - long

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,6 +1,11 @@
 package main
 
-import "sync"
+import (
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
 
 //func main() {
 //
@@ -8,8 +13,9 @@ import "sync"
 
 func Worker(job job, in, out chan interface{}, wg *sync.WaitGroup) {
 	defer wg.Done()
-	defer close(out)
+	//defer close(out)
 	job(in, out)
+	close(out)
 }
 
 func ExecutePipeline(jobs ...job) {
@@ -28,13 +34,31 @@ func ExecutePipeline(jobs ...job) {
 }
 
 func SingleHash(in, out chan interface{}) {
-
+	for data := range in {
+		d := strconv.Itoa(data.(int))
+		s := DataSignerCrc32(d) + "~" + DataSignerCrc32(DataSignerMd5(d))
+		//fmt.Println(data, s)
+		out <- s
+	}
 }
 
 func MultiHash(in, out chan interface{}) {
-
+	for data := range in {
+		s := ""
+		for i := 0; i < 6; i++ {
+			s += DataSignerCrc32(strconv.Itoa(i) + data.(string))
+		}
+		//fmt.Println(data, s)
+		out <- s
+	}
 }
 
 func CombineResults(in, out chan interface{}) {
+	slice := make([]string, 0)
+	for data := range in {
+		slice = append(slice, data.(string))
 
+	}
+	sort.Strings(slice)
+	out <- strings.Join(slice, "_")
 }
