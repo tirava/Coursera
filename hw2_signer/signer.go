e6ea7a61cb022dd7cf89bf29d67d8d7e7b3a747a
Eugene Klimov <tirava@gmail.com>
2019-07-08 22:21:30 +0300

    Pipeline candidate 1 - 3.07

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -34,56 +34,76 @@ func ExecutePipeline(jobs ...job) {
 	wg.Wait()
 }
 
+func SingleHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
+
+	for i := range in {
+		wg.Add(1)
+		go singleHashJob(i, out, wg, mu)
+	}
+	wg.Wait()
+}
+
+func singleHashJob(in interface{}, out chan interface{}, wg *sync.WaitGroup, mu *sync.Mutex) {
+	defer wg.Done()
+	data := strconv.Itoa(in.(int))
+
+	mu.Lock()
+	md5Data := DataSignerMd5(data)
+	mu.Unlock()
+
+	crc32Chan := make(chan string)
+	go crc32Worker(data, crc32Chan)
+
+	crc32Data := <-crc32Chan
+	crc32Md5Data := DataSignerCrc32(md5Data)
+
+	out <- crc32Data + "~" + crc32Md5Data
+}
+
 func crc32Worker(data string, out chan string) {
 	out <- DataSignerCrc32(data)
 }
 
-func SingleHash(in, out chan interface{}) {
-	ch := make(chan string)
-	ch5 := make(chan string)
-	i := len(in)
-	for data := range in {
-		d := strconv.Itoa(data.(int))
-		//s := DataSignerCrc32(d) + "~" + DataSignerCrc32(DataSignerMd5(d))
-		go crc32Worker(d, ch)
-		go crc32Worker(DataSignerMd5(d), ch5)
-		//s := <-ch + "~" + <-ch5
-		//fmt.Println(data, s)
-		//out <- s
+func MultiHash(in, out chan interface{}) {
+	const TH int = 6
+	wg := &sync.WaitGroup{}
+
+	for i := range in {
+		wg.Add(1)
+		go multiHashJob(i.(string), out, TH, wg)
 	}
-	for j := 0; j < i; j++ {
-		s := <-ch + "~" + <-ch5
-		fmt.Println(s)
-		out <- s
-		//println("out:", s)
-	}
-	//println("------------------------")
+	wg.Wait()
 }
 
-func MultiHash(in, out chan interface{}) {
-	mch := make(map[int]chan string, MaxInputDataLen)
-	//ch := make(chan string)
-	for data := range in {
-		s := ""
-		for i := 0; i < 6; i++ {
-			mch[i] = make(chan string)
-			go crc32Worker(strconv.Itoa(i)+data.(string), mch[i])
-			//s += <-ch
-			//s += DataSignerCrc32(strconv.Itoa(i) + data.(string))
-		}
-		for i := 0; i < 6; i++ {
-			s += <-mch[i]
-		}
-		fmt.Println(data, s)
-		out <- s
-		//println("out:", s)
+func multiHashJob(in string, out chan interface{}, th int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	mu := &sync.Mutex{}
+	jobWg := &sync.WaitGroup{}
+	combinedChunks := make([]string, th)
+
+	for i := 0; i < th; i++ {
+		jobWg.Add(1)
+		data := strconv.Itoa(i) + in
+
+		go func(acc []string, index int, data string, jobWg *sync.WaitGroup, mu *sync.Mutex) {
+			defer jobWg.Done()
+			data = DataSignerCrc32(data)
+
+			mu.Lock()
+			acc[index] = data
+			mu.Unlock()
+		}(combinedChunks, i, data, jobWg, mu)
 	}
-	//println("------------------------")
+
+	jobWg.Wait()
+	out <- strings.Join(combinedChunks, "")
 }
 
 func CombineResults(in, out chan interface{}) {
-	slice := make([]string, 0)
-	//println("lenRes:", len(in))
+	var slice []string
 	for data := range in {
 		slice = append(slice, data.(string))
 	}
