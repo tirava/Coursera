8df25a3a9bbf34646f3f7c22ae58cc4b632d6ad3
Eugene Klimov <tirava@gmail.com>
2019-07-08 22:31:53 +0300

    My variant 8 sec

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -39,45 +39,43 @@ func crc32Worker(data string, out chan string) {
 }
 
 func SingleHash(in, out chan interface{}) {
-	//ch := make(chan string)
-	//ch5 := make(chan string)
-	//i := len(in)
+	ch := make(chan string)
+	ch5 := make(chan string)
+	i := len(in)
 	for data := range in {
-		out <- strconv.Itoa(data.(int))
-		//d := strconv.Itoa(data.(int))
+		d := strconv.Itoa(data.(int))
 		//s := DataSignerCrc32(d) + "~" + DataSignerCrc32(DataSignerMd5(d))
-		//go crc32Worker(d, ch)
-		//go crc32Worker(DataSignerMd5(d), ch5)
+		go crc32Worker(d, ch)
+		go crc32Worker(DataSignerMd5(d), ch5)
 		//s := <-ch + "~" + <-ch5
 		//fmt.Println(data, s)
 		//out <- s
 	}
-	//for j := 0; j < i; j++ {
-	//	s := <-ch + "~" + <-ch5
-	//	fmt.Println(s)
-	//	out <- s
-	//	//println("out:", s)
-	//}
+	for j := 0; j < i; j++ {
+		s := <-ch + "~" + <-ch5
+		fmt.Println(s)
+		out <- s
+		//println("out:", s)
+	}
 	//println("------------------------")
 }
 
 func MultiHash(in, out chan interface{}) {
-	//mch := make(map[int]chan string, MaxInputDataLen)
+	mch := make(map[int]chan string, MaxInputDataLen)
 	//ch := make(chan string)
 	for data := range in {
-		out <- data
-		//s := ""
-		//for i := 0; i < 6; i++ {
-		//	mch[i] = make(chan string)
-		//	go crc32Worker(strconv.Itoa(i)+data.(string), mch[i])
-		//	//s += <-ch
-		//	//s += DataSignerCrc32(strconv.Itoa(i) + data.(string))
-		//}
-		//for i := 0; i < 6; i++ {
-		//	s += <-mch[i]
-		//}
-		//fmt.Println(data, s)
-		//out <- s
+		s := ""
+		for i := 0; i < 6; i++ {
+			mch[i] = make(chan string)
+			go crc32Worker(strconv.Itoa(i)+data.(string), mch[i])
+			//s += <-ch
+			//s += DataSignerCrc32(strconv.Itoa(i) + data.(string))
+		}
+		for i := 0; i < 6; i++ {
+			s += <-mch[i]
+		}
+		fmt.Println(data, s)
+		out <- s
 		//println("out:", s)
 	}
 	//println("------------------------")
@@ -85,15 +83,9 @@ func MultiHash(in, out chan interface{}) {
 
 func CombineResults(in, out chan interface{}) {
 	slice := make([]string, 0)
+	//println("lenRes:", len(in))
 	for data := range in {
-		d := data.(string)
-		s := DataSignerCrc32(d) + "~" + DataSignerCrc32(DataSignerMd5(d))
-		sm := ""
-		for i := 0; i < 6; i++ {
-			sm += DataSignerCrc32(strconv.Itoa(i) + s)
-			println(sm)
-		}
-		slice = append(slice, sm)
+		slice = append(slice, data.(string))
 	}
 	sort.Strings(slice)
 	result := strings.Join(slice, "_")
