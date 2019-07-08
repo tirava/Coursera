30a3e48a4089f079fefefa271888e95211d092fc
Eugene Klimov <tirava@gmail.com>
2019-07-08 23:36:36 +0300

    My variant 2.08 sec - best!

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -44,46 +44,43 @@ func SingleHash(in, out chan interface{}) {
 	i := len(in)
 	for data := range in {
 		d := strconv.Itoa(data.(int))
-		//s := DataSignerCrc32(d) + "~" + DataSignerCrc32(DataSignerMd5(d))
 		go crc32Worker(d, ch)
 		go crc32Worker(DataSignerMd5(d), ch5)
-		//s := <-ch + "~" + <-ch5
-		//fmt.Println(data, s)
-		//out <- s
 	}
 	for j := 0; j < i; j++ {
 		s := <-ch + "~" + <-ch5
-		fmt.Println(s)
 		out <- s
-		//println("out:", s)
 	}
-	//println("------------------------")
+}
+
+func crc32WorkerM(data string, out chan string) {
+	mch := make(map[int]chan string, MaxInputDataLen)
+	s := ""
+	for i := 0; i < 6; i++ {
+		mch[i] = make(chan string)
+		go crc32Worker(strconv.Itoa(i)+data, mch[i])
+	}
+	for i := 0; i < 6; i++ {
+		s += <-mch[i]
+	}
+	out <- s
 }
 
 func MultiHash(in, out chan interface{}) {
-	mch := make(map[int]chan string, MaxInputDataLen)
-	//ch := make(chan string)
+	ch := make(chan string, MaxInputDataLen)
+	i := 0
 	for data := range in {
-		s := ""
-		for i := 0; i < 6; i++ {
-			mch[i] = make(chan string)
-			go crc32Worker(strconv.Itoa(i)+data.(string), mch[i])
-			//s += <-ch
-			//s += DataSignerCrc32(strconv.Itoa(i) + data.(string))
-		}
-		for i := 0; i < 6; i++ {
-			s += <-mch[i]
-		}
-		fmt.Println(data, s)
-		out <- s
-		//println("out:", s)
+		go crc32WorkerM(data.(string), ch)
+		i++
 	}
-	//println("------------------------")
+	for j := 0; j < i; j++ {
+		s := <-ch
+		out <- s
+	}
 }
 
 func CombineResults(in, out chan interface{}) {
 	slice := make([]string, 0)
-	//println("lenRes:", len(in))
 	for data := range in {
 		slice = append(slice, data.(string))
 	}
