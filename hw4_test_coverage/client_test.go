e6783f19e12c60c96ad08e16628b38f1a5ad82a6
Eugene Klimov <tirava@gmail.com>
2019-07-18 23:54:16 +0300

    HomeWork-4 very draft

--- hw4_test_coverage/client_test.go
+++ hw4_test_coverage/client_test.go
@@ -1,3 +1,38 @@
 package main
 
-// код писать тут
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SearchServer(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func TestSearchClient(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	sc := &SearchClient{
+		"111",
+		ts.URL,
+	}
+
+	sr := SearchRequest{
+		5,
+		1,
+		"555",
+		"name",
+		0,
+	}
+
+	result, err := sc.FindUsers(sr)
+	if err != nil {
+		t.Errorf("klim error: %#v", err)
+	}
+
+	fmt.Println(result)
+
+	ts.Close()
+}
