60cd54c21e046c0c650e0955cb1fe10573db7675
Eugene Klimov <tirava@gmail.com>
2019-07-19 22:38:41 +0300

    HomeWork-4 draft 69%

--- hw4_test_coverage/client_test.go
+++ hw4_test_coverage/client_test.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func SearchServer(w http.ResponseWriter, r *http.Request) {
+	status := r.URL.Query().Get("query")
+	orderField := r.URL.Query().Get("order_field")
+	switch status {
+	case "StatusInternalServerError":
+		w.WriteHeader(http.StatusInternalServerError)
+	case "StatusBadRequest":
+		w.WriteHeader(http.StatusBadRequest)
+		switch orderField {
+		case "name":
+			w.Write([]byte(`{"Error":"ErrorBadOrderField"}`))
+		case "name1":
+			w.Write([]byte(`{"Error":"ErrorBadOther"}`))
+		default:
+			w.Write([]byte(`111`))
+		}
+	case "StatusUnauthorized":
+		w.WriteHeader(http.StatusUnauthorized)
+	default:
+		//parse user
+	}
 
+	//fmt.Println(r.URL.Query())
 }
 
 func TestSearchClient(t *testing.T) {
@@ -19,20 +39,67 @@ func TestSearchClient(t *testing.T) {
 		ts.URL,
 	}
 
-	sr := SearchRequest{
-		5,
-		1,
-		"555",
-		"name",
-		0,
+	srs := []SearchRequest{
+		{
+			-1,
+			1,
+			"555",
+			"name",
+			0,
+		},
+		{
+			35,
+			1,
+			"StatusInternalServerError",
+			"name",
+			0,
+		},
+		{
+			1,
+			-1,
+			"555",
+			"name",
+			0,
+		},
+		{
+			5,
+			1,
+			"StatusBadRequest",
+			"name",
+			0,
+		},
+		{
+			5,
+			1,
+			"StatusBadRequest",
+			"name1",
+			0,
+		},
+		{
+			5,
+			1,
+			"StatusBadRequest",
+			"nameX",
+			0,
+		},
+		{
+			5,
+			1,
+			"StatusUnauthorized",
+			"name",
+			0,
+		},
 	}
 
-	result, err := sc.FindUsers(sr)
-	if err != nil {
-		t.Errorf("klim error: %#v", err)
-	}
+	for _, sr := range srs {
 
-	fmt.Println(result)
+		_, err := sc.FindUsers(sr)
+		if err != nil {
+			//t.Errorf("klim error: %#v", err)
+		}
+
+		//fmt.Println(result)
+	}
 
 	ts.Close()
 }
