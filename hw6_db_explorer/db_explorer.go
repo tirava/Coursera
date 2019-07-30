f42f231321589bf29e06dde69c8dc5b0fe2dd4a8
Eugene Klimov <tirava@gmail.com>
2019-07-30 23:58:08 +0300

    HomeWork-6 work Case [1]

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -4,9 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"path"
+	"strings"
 )
 
 // тут вы пишете код
@@ -16,16 +16,21 @@ type pathResolver struct {
 	handlers map[string]http.HandlerFunc
 }
 
+type cr map[string]interface{}
+
 type crTables map[string]map[string][]string
 
 func NewDbExplorer(db *sql.DB) (handler *pathResolver, err error) {
 	fooGetTables := func(w http.ResponseWriter, r *http.Request) {
 		getTables(w, r, db)
 	}
+	fooGetTable := func(w http.ResponseWriter, r *http.Request) {
+		getTable(w, r, db)
+	}
 	pr := newPathResolver()
-	//pr.Add("GET /hello", hello)
 	//pr.Add("* /goodbye/*", goobye)
 	pr.Add("GET /", fooGetTables)
+	pr.Add("GET /*", fooGetTable)
 	return pr, nil
 }
 
@@ -55,17 +60,16 @@ func getTables(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if err != nil {
 		//log.Fatal("can't show tables", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
-	tables := make([]string, 0)
-	table := ""
-	for rows.Next() {
-		//err = rows.Scan(&table)
-		err = rows.Scan(&table)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Fatal("can't scan rows", err)
-		}
-		tables = append(tables, table)
+	// надо закрывать соединение, иначе будет течь
+	defer rows.Close()
+
+	tables, err := listTables(rows)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		//log.Fatal("can't scan rows", err)
+		return
 	}
 
 	result := crTables{}
@@ -78,7 +82,59 @@ func getTables(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 	w.Write([]byte(jsonTables))
+}
 
+func listTables(rows *sql.Rows) (tables []string, err error) {
+	//tables := make([]string, 0)
+	table := ""
+	for rows.Next() {
+		//err = rows.Scan(&table)
+		err := rows.Scan(&table)
+		if err != nil {
+			return nil, err
+			//log.Fatal("can't scan rows", err)
+		}
+		tables = append(tables, table)
+	}
+	return tables, nil
+}
+
+func getTable(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	rows, err := db.Query("SHOW TABLES;")
+	if err != nil {
+		//log.Fatal("can't show tables", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 	// надо закрывать соединение, иначе будет течь
-	rows.Close()
+	defer rows.Close()
+
+	tables, err := listTables(rows)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		//log.Fatal("can't scan rows", err)
+	}
+
+	path := r.URL.Path
+	parts := strings.Split(path, "/")
+	name := parts[1]
+
+	found := false
+	for _, table := range tables {
+		if table == name {
+			found = true
+			break
+		}
+	}
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
+		result := &cr{"error": "unknown table"}
+		jsonResult, err := json.Marshal(result)
+		if err != nil {
+			//log.Fatal("can't marshal tables", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Write([]byte(jsonResult))
+		return
+	}
+
 }
