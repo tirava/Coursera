247352e1267caf55921a2c5461e030c4395727e6
Eugene Klimov <tirava@gmail.com>
2019-08-04 15:47:30 +0300

    HomeWork-7 start - protoc ready

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -1,4 +1,11 @@
 package main
 
+import "context"
+
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
+
+func StartMyMicroservice(ctx context.Context, listenAddr, ACLData string) (err error) {
+
+	return nil
+}
