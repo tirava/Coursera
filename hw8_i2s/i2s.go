3deb008055d61e7a6e863502784aba1fa3c2ba8a
Eugene Klimov <tirava@gmail.com>
2019-08-09 21:15:17 +0300

    HomeWork-8 End

--- hw8_i2s/i2s.go
+++ hw8_i2s/i2s.go
@@ -1,91 +1,75 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"errors"
 	"reflect"
 )
 
 func i2s(data interface{}, out interface{}) error {
 
-	//elems := reflect.ValueOf(data).MapRange()
-	//elemsType := elems.Type()
-	//fmt.Println().Println(elems)
+	outPtr := reflect.ValueOf(out)
+	if outPtr.Kind() != reflect.Ptr {
+		return errors.New("need Ptr only")
+	}
+	outElem := outPtr.Elem()
 
-	//val := reflect.ValueOf(data)
-	//fmt.Println(data.(type))
-	//fmt.Println("VALUE = ", val)
-	//fmt.Println("KIND = ", val.Kind())
-	//switch data.(type) {
-	//default:
-	//case map[string]interface{}:
-	//	println("mmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
-	//}
+	switch outElem.Kind() {
 
-	//switch val.Kind() {
-	//case reflect.Map:
-	//	iter := reflect.ValueOf(data).MapRange()
-	//	for iter.Next() {
-	//		k := iter.Key()
-	//		v := iter.Value()
-	//		//t := v.Type()
-	//		fmt.Println("kkk:", k, "vvv:", v, "ttt:")
-	//		v.Convert(reflect.Int)
-	//	}
-	//for _, e := range val.MapKeys() {
-	//	v := val.MapIndex(e)
-	//	switch t := v.Interface().(type) {
-	//	case int:
-	//		fmt.Println(e, t)
-	//	case string:
-	//		fmt.Println(e, t)
-	//	case bool:
-	//		fmt.Println(e, t)
-	//	default:
-	//		fmt.Println("not found")
-	//
-	//	}
-	//}
-	//println("mapppppppppp")
-	//case reflect.Slice:
-	//	//println("sliceeeeeeeeeeeeee")
-	//default:
-	//	//println("otherssssssssssssssssssss")
-	//}
+	case reflect.Struct:
+		mapI, ok := data.(map[string]interface{})
+		if !ok {
+			return errors.New("need map interface")
+		}
+		for k, v := range mapI {
+			o := reflect.New(outElem.FieldByName(k).Type()).Interface()
+			err := i2s(v, o)
+			if err != nil {
+				return errors.New("error convert to struct")
+			}
+			outElem.FieldByName(k).Set(reflect.ValueOf(o).Elem())
+		}
 
-	result, _ := json.Marshal(data)
-	//fmt.Println("json:", string(result))
-	json.Unmarshal(result, out)
+	case reflect.Slice:
+		sliceI, ok := data.([]interface{})
+		if !ok {
+			return errors.New("need slice")
+		}
+		elems := reflect.New(outElem.Type()).Elem()
+		for _, s := range sliceI {
+			o := reflect.New(outElem.Type().Elem()).Interface()
+			err := i2s(s, o)
+			if err != nil {
+				return errors.New("error convert to struct")
+			}
+			elems = reflect.Append(elems, reflect.ValueOf(o).Elem())
+		}
+		outElem.Set(elems)
 
-	fmt.Println(reflect.ValueOf(out).Type())
+	case reflect.Int:
+		val, ok := data.(float64)
+		if !ok {
+			return errors.New("need int")
+		}
+		outElem.SetInt(int64(val))
 
-	//return errors.New("111")
+	case reflect.String:
+		val, ok := data.(string)
+		if !ok {
+			return errors.New("need string")
+		}
+		outElem.SetString(val)
+
+	case reflect.Bool:
+		val, ok := data.(bool)
+		if !ok {
+			return errors.New("need bool")
+		}
+		outElem.SetBool(val)
+
+	default:
+		return errors.New("unknown error")
+
+	}
+
 	return nil
 }
-
-//func CreateMap(key, elem reflect.Type) reflect.Value {
-//	var mapType reflect.Type
-//	mapType = reflect.MapOf(key, elem)
-//	return reflect.MakeMap(mapType)
-//}
-
-/*var jsonStr = `[
-	{"id": 17, "username": "iivan", "phone": 0},
-	{"id": "17", "address": "none", "company": "Mail.ru"}
-]`*/
-
-/*func main() {
-	data := []byte(jsonStr)
-
-	var user1 interface{}
-	json.Unmarshal(data, &user1)
-	fmt.Printf("unpacked in empty interface:\n%#v\n\n", user1)
-
-	user2 := map[string]interface{}{
-		"id":       42,
-		"username": "rvasily",
-	}
-	var user2i interface{} = user2
-	result, _ := json.Marshal(user2i)
-	fmt.Printf("json string from map:\n %s\n", string(result))
-}*/
